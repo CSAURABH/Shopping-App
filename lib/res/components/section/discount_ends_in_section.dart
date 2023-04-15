d8a5bb0d5255f1272f451f1d0b717a5e2c9f8999
CSAURABH <578saurabh@gmail.com>
2023-04-15 19:59:31 +0530

    create new file for discount ends in section

---
+++ lib/res/components/section/discount_ends_in_section.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../style/app_styles.dart';
+import '../../style/color.dart';
+import '../../style/svg.dart';
+import '../title_heading.dart';
+
+class DiscountEndsInSection extends StatelessWidget {
+  const DiscountEndsInSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            const TitleHeading(title: 'Discount ends in'),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6.sp),
+              height: 19.h,
+              width: 19.w,
+              color: AppColors.blackColor,
+              child: Center(
+                child: Text(
+                  '02',
+                  style:
+                      AppStyles.fontSize9.copyWith(color: AppColors.whiteColor),
+                ),
+              ),
+            ),
+            SvgPicture.asset(SvgIcons.colon),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6.sp),
+              height: 19.h,
+              width: 19.w,
+              color: AppColors.blackColor,
+              child: Center(
+                child: Text(
+                  '24',
+                  style:
+                      AppStyles.fontSize9.copyWith(color: AppColors.whiteColor),
+                ),
+              ),
+            ),
+            SvgPicture.asset(SvgIcons.colon),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6.sp),
+              height: 19.h,
+              width: 19.w,
+              color: AppColors.blackColor,
+              child: Center(
+                child: Text(
+                  '09',
+                  style:
+                      AppStyles.fontSize9.copyWith(color: AppColors.whiteColor),
+                ),
+              ),
+            ),
+            const Spacer(),
+            Text(
+              'See all',
+              style: AppStyles.fontSize12.copyWith(color: AppColors.blue),
+            )
+          ],
+        ),
+        SizedBox(height: 15.h),
+        SizedBox(
+          height: 150.h,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (context, index) => Container(
+              width: 125.w,
+              margin: EdgeInsets.only(right: 5.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 100.h,
+                    width: double.infinity,
+                    child: const Image(
+                      image: AssetImage(
+                        'assets/images/onboarding1.jpg',
+                      ),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Donatello',
+                                style: AppStyles.fontSize9w400.copyWith(
+                                  color: AppColors.blackColor,
+                                ),
+                              ),
+                              Text(
+                                'Cream elegant',
+                                style: AppStyles.fontSize9w400.copyWith(
+                                  color: AppColors.lightGrey,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SvgPicture.asset(
+                            SvgIcons.unSavedHeart,
+                            height: 15.h,
+                          )
+                        ],
+                      ),
+                      Text(
+                        '\$ 398.90',
+                        style: AppStyles.fontSize9w400,
+                      ),
+                      Text(
+                        '\$ 402.90',
+                        style: AppStyles.fontSize9w400.copyWith(
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 16.h),
+      ],
+    );
+  }
+}
--- lib/view/dashboard_screen.dart
+++ lib/view/dashboard_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:salvage_shoping_app/res/components/common_heading_and_see_all_function.dart';
+import 'package:salvage_shoping_app/res/components/section/discount_ends_in_section.dart';
 import 'package:salvage_shoping_app/res/components/section/new_in_bags_section.dart';
 import 'package:salvage_shoping_app/res/components/section/new_in_clothes_section.dart';
 import 'package:salvage_shoping_app/res/components/section/new_in_electronics_section.dart';
@@ -125,126 +126,7 @@ class _HomePageState extends State<DashBoardScreen> {
               ),
               SizedBox(height: 10.h),
               //Discount ends in Section
-              Row(
-                children: [
-                  const TitleHeading(title: 'Discount ends in'),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 6.sp),
-                    height: 19.h,
-                    width: 19.w,
-                    color: AppColors.blackColor,
-                    child: Center(
-                      child: Text(
-                        '02',
-                        style: AppStyles.fontSize9
-                            .copyWith(color: AppColors.whiteColor),
-                      ),
-                    ),
-                  ),
-                  SvgPicture.asset(SvgIcons.colon),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 6.sp),
-                    height: 19.h,
-                    width: 19.w,
-                    color: AppColors.blackColor,
-                    child: Center(
-                      child: Text(
-                        '24',
-                        style: AppStyles.fontSize9
-                            .copyWith(color: AppColors.whiteColor),
-                      ),
-                    ),
-                  ),
-                  SvgPicture.asset(SvgIcons.colon),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 6.sp),
-                    height: 19.h,
-                    width: 19.w,
-                    color: AppColors.blackColor,
-                    child: Center(
-                      child: Text(
-                        '09',
-                        style: AppStyles.fontSize9
-                            .copyWith(color: AppColors.whiteColor),
-                      ),
-                    ),
-                  ),
-                  const Spacer(),
-                  Text(
-                    'See all',
-                    style: AppStyles.fontSize12.copyWith(color: AppColors.blue),
-                  )
-                ],
-              ),
-              SizedBox(height: 15.h),
-              SizedBox(
-                height: 150.h,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) => Container(
-                    width: 125.w,
-                    margin: EdgeInsets.only(right: 5.w),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 100.h,
-                          width: double.infinity,
-                          child: const Image(
-                            image: AssetImage(
-                              'assets/images/onboarding1.jpg',
-                            ),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Donatello',
-                                      style: AppStyles.fontSize9w400.copyWith(
-                                        color: AppColors.blackColor,
-                                      ),
-                                    ),
-                                    Text(
-                                      'Cream elegant',
-                                      style: AppStyles.fontSize9w400.copyWith(
-                                        color: AppColors.lightGrey,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SvgPicture.asset(
-                                  SvgIcons.unSavedHeart,
-                                  height: 15.h,
-                                )
-                              ],
-                            ),
-                            Text(
-                              '\$ 398.90',
-                              style: AppStyles.fontSize9w400,
-                            ),
-                            Text(
-                              '\$ 402.90',
-                              style: AppStyles.fontSize9w400.copyWith(
-                                decoration: TextDecoration.lineThrough,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.h),
+              const DiscountEndsInSection(),
               //Upcomming promotion section
               const UpComingPromotionSection(),
               // New in Shoes Section
