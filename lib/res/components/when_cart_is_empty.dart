9aee9b72e23c2967d376cb6eea8598db7518f2bb
CSAURABH <578saurabh@gmail.com>
2023-04-16 20:58:41 +0530

    when wishlist is empty ui completed

---
+++ lib/res/components/when_cart_is_empty.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:salvage_shoping_app/res/style/app_styles.dart';
+import 'package:salvage_shoping_app/res/style/color.dart';
+import 'package:salvage_shoping_app/res/style/svg.dart';
+
+class WhenCartIsEmptyScreen extends StatelessWidget {
+  const WhenCartIsEmptyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(width: double.infinity),
+        SvgPicture.asset(
+          SvgIcons.unSavedHeart,
+          height: 78.h,
+          color: AppColors.grey,
+        ),
+        SizedBox(height: 10.h),
+        Text(
+          'Your wishlist is empty',
+          style: AppStyles.fontSize16.copyWith(color: AppColors.icongrey),
+        ),
+        SizedBox(height: 12.h),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.blue,
+          ),
+          onPressed: () {},
+          child: Text(
+            'SHOP NOW',
+            style: AppStyles.fontSize12,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/view/wish_list_screen.dart
+++ lib/view/wish_list_screen.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:salvage_shoping_app/res/components/when_cart_is_empty.dart';
 
 class WishListScreen extends StatelessWidget {
   const WishListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WishList Screen'),
-        centerTitle: true,
-      ),
-    );
+    return const WhenCartIsEmptyScreen();
   }
 }
